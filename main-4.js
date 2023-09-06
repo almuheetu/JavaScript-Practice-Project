3308c727b733ff2c42cbddafc987c8892d033710
Almuheetu Shihab <almuheetu@gmail.com>
2023-09-06 16:25:27 +0600

    JavaScript Stop watch finished

--- main-4.js
+++ main-4.js
@@ -5,8 +5,40 @@ var OutputTens = document.getElementById('tens')
 var buttonStart = document.getElementById('btn-start')
 var buttonStop = document.getElementById('btn-stop')
 var buttonReset = document.getElementById('btn-reset')
+var Interval;
 
 buttonStart.addEventListener('click', () => {
     clearInterval(Interval);
     Interval = setInterval(startTime, 10);
 });
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(Interval);
+});
+
+buttonReset.addEventListener('click', () => {
+    clearInterval(Interval);
+    tens = "00";
+    seconds = "00";
+    OutputSeconds.innerHTML = seconds;
+    OutputTens.innerHTML = tens;
+});
+
+function startTime(){
+    tens++;
+    if(tens <= 9){
+        OutputSeconds.innerHTML = "0" + tens;  
+    }
+    if(tens > 9){
+        OutputTens.innerHTML = tens;  
+    }
+    if(tens > 99){
+        seconds++;
+        OutputSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        OutputTens.innerHTML = "0" + tens;
+    }
+    if(tens > 9){
+        OutputSeconds.innerHTML = seconds;
+    }
+}
