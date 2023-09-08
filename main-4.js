8b1babbaefb0399b52610d3f69b1f5e4e8a57e4c
Almuheetu Shihab <almuheetu@gmail.com>
2023-09-08 16:07:00 +0600

    stopwatch fix

--- main-4.js
+++ main-4.js
@@ -1,44 +1,72 @@
-var seconds = '00';
-var tens = '00';
-var OutputSeconds = document.getElementById('second')
-var OutputTens = document.getElementById('tens')
+let [seconds, minutes, hours] = [0, 0, 0];
+var displayTime = document.getElementById("displayTime")
 var buttonStart = document.getElementById('btn-start')
 var buttonStop = document.getElementById('btn-stop')
 var buttonReset = document.getElementById('btn-reset')
-var Interval;
+var Interval = null;
 
-buttonStart.addEventListener('click', () => {
-    clearInterval(Interval);
-    Interval = setInterval(startTime, 10);
-});
+// buttonStart.addEventListener('click', () => {
+//     if (Interval !== null) {
+//         clearInterval(Interval);
+//     }
 
-buttonStop.addEventListener('click', () => {
-    clearInterval(Interval);
-});
+//     Interval = setInterval(startTime, 1000);
+// });
 
-buttonReset.addEventListener('click', () => {
-    clearInterval(Interval);
-    tens = "00";
-    seconds = "00";
-    OutputSeconds.innerHTML = seconds;
-    OutputTens.innerHTML = tens;
-});
 
-function startTime(){
-    tens++;
-    if(tens <= 9){
-        OutputSeconds.innerHTML = "0" + tens;  
+// buttonStop.addEventListener('click', () => {
+//     clearInterval(Interval);
+// });
+
+// buttonReset.addEventListener('click', () => {
+//     clearInterval(Interval);
+//     minutes = "00";
+//     seconds = "00";
+//     hours = "00";
+//     OutputSeconds.innerHTML = seconds;
+//     OutputMinutes.innerHTML = minutes;
+//     OutputHours.innerHTML = hours;
+// });
+
+function startTime() {
+    seconds++;
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes == 60) {
+            minutes = 0;
+            hours++;
+        }
     }
-    if(tens > 9){
-        OutputTens.innerHTML = tens;  
-    }
-    if(tens > 99){
-        seconds++;
-        OutputSeconds.innerHTML = "0" + seconds;
-        tens = 0;
-        OutputTens.innerHTML = "0" + tens;
-    }
-    if(tens > 9){
-        OutputSeconds.innerHTML = seconds;
-    }
+
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+
+    displayTime.innerHTML = h + ":" + m + ":" + s;
 }
+
+function watchStart() {
+    if (Interval !== null) {
+        clearInterval(Interval);
+    }
+
+    Interval = setInterval(startTime, 1000);
+}
+
+
+function watchStop() {
+    clearInterval(Interval);
+}
+
+
+function watchReset() {
+    clearInterval(Interval);
+    [seconds, minutes, hours] = [0, 0, 0];
+    displayTime.innerHTML = "00:00:00";
+}
+
+
+
